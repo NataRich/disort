3d40d70b9c41db29c20926dd986218684c0fd327
Zihan Zhao <natarichjay@gmail.com>
2023-02-13 14:06:38 -0800

    feat: use files funcs

--- src/cnode.c
+++ src/cnode.c
@@ -4,20 +4,20 @@
 #include <stdlib.h>
 
 #include "net.h"
-#include "proto.h"
+#include "files.h"
 #include "utils.h"
 
 // static const struct node dnode = {.addr = "127.0.0.1", .port = 8000};
 
 int main(int argc, char *argv[])
 {
-    int sockfd, ret;
+    int sockfd;
     u_int16_t port;
     struct sockaddr_in client;
 
-    if (argc != 2)
+    if (argc != 3)
     {
-        error("Usage: %s [Port]\n", argv[0]);
+        error("Usage: %s [Port] [Path]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
@@ -25,14 +25,21 @@ int main(int argc, char *argv[])
     if (port < 1 || port > 65535)
     {
         error("[Error]: Port number must be between 1 and 65535\n");
+        error("Usage: %s [Port] [Path]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (fexists(argv[2]) == 0)
+    {
+        error("[Error]: File (%s) already exists\n", argv[2]);
+        error("Usage: %s [Port] [Path]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     sockfd = init_server(port);
     sockfd = accept_conn(sockfd, &client);
-    ret = reply(sockfd);
-    if (ret < 0)
-        info("[Info]: Oops!\n");
+    freceive(sockfd, argv[2]);
+    close(sockfd);
 
     exit(EXIT_SUCCESS);
 }
--- src/dnode.c
+++ src/dnode.c
@@ -5,7 +5,7 @@
 #include <pthread.h>
 
 #include "net.h"
-#include "proto.h"
+#include "files.h"
 #include "utils.h"
 
 static const struct node cnodes[] = {
@@ -13,23 +13,44 @@ static const struct node cnodes[] = {
         .addr = "127.0.0.1",
         .port = 7000,
     },
-    {
-        .addr = "127.0.0.1",
-        .port = 7001,
-    },
 };
 
 int main(int argc, char *argv[])
 {
-    int sockfd;
-    struct packet meta;
+    int i, sockfd, ncnt;
+    u_int16_t client_port;
+    char client_ip[30];
 
-    sockfd = init_client(cnodes[0].addr, cnodes[0].port);
-    meta.seq = 0;
-    meta.size = 10;
-    meta.flags = LFM_F_FRST;
-    if (confirm(sockfd, &meta, 2) < 0)
-        info("[Info]: Oops!\n");
+    ncnt = sizeof(cnodes) / sizeof(struct node);
+    if (argc != ncnt + 1)
+    {
+        error("[Error]: Incorrect number of data files and nodes\n");
+        error("Usage: %s [PATHS...]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    for (i = 0; i < ncnt; i++)
+    {
+        if (fexists(argv[i + 1]) != 0)
+        {
+            error("[Error]: File (%s) does not exist\n", argv[i + 1]);
+            error("Usage: %s [PATHS...]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (i = 0; i < ncnt; i++)
+    {
+        memcpy(client_ip, cnodes[i].addr, 30);
+        client_port = cnodes[i].port;
+        sockfd = init_client(client_ip, client_port);
+        if (ftransfer(sockfd, argv[i + 1]) < 0)
+        {
+            info("[Info]: Failed to transfer file (%s) to mode (%s:%d)\n",
+                 argv[i + 1], client_ip, client_port);
+        }
+        close(sockfd);
+    }
 
     exit(EXIT_SUCCESS);
 }
